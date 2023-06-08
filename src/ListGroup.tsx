b805abab609bb59075123d48a6b378875d76cede
Carlos Ferreira <carlos@especializati.com.br>
2023-06-08 12:05:37 -0300

    feat: handling events (and stop event default)

--- src/ListGroup.tsx
+++ src/ListGroup.tsx
@@ -13,7 +13,13 @@ function ListGroup () {
             <div className="max-w-lg mx-auto">
                 <div className="bg-white rounded-lg border border-gray-200 w-48 text-gray-900 text-sm font-medium">
                     {items.map((item, index) => (
-                        <a href="" key={index} className="block px-4 py-2 border-b border-gray-200 w-full rounded-b-lg hover:bg-gray-100 hover:text-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-700 focus:text-blue-700 cursor-pointer">{item}</a>
+                        <a href=""
+                            key={index}
+                            onClick={(e) => {
+                                e.preventDefault();
+                                console.log(item);
+                            }}
+                            className="block px-4 py-2 border-b border-gray-200 w-full rounded-b-lg hover:bg-gray-100 hover:text-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-700 focus:text-blue-700 cursor-pointer">{item}</a>
                     ))}
                 </div>    
             </div>
